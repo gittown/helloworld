6cab7bbe482da7e9b8be95b354d60bd36a625ab6
gittown <gittown@gittownrovers.com>
2020-05-31 17:10:14 +0100

    Changed Pierre to Jean-Pierre

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ import util.*;
 public class HelloWorld{
   public static void main(String[]args){
     System.out.println("*** Hello World! ***");
-    System.out.println("Pierre says "+Util.translateToFrench("Hello World!"));
+    System.out.println("Jean-Pierre says "+Util.translateToFrench("Hello World!"));
   }
 }
