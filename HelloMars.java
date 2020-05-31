e64172078982bc86c88df8b1a883ef0a3ebc7cd0
gittown <gittown@gittownrovers.com>
2020-05-31 18:50:09 +0100

    PULL REQ!!!

--- HelloMars.java
+++ HelloMars.java
@@ -1,5 +1,5 @@
 public class HelloMars{
   public static void main(String[]args){
-    System.out.println("PULL REQ!! -- ☺ Hello Mars! ☺ --");
+    System.out.println("PULL REQ!!! -- ☺ Hello Mars! ☺ --");
   }
 }
